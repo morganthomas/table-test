2b0af587d93fffdc53e95690f92cf41a702a33f2
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-08 23:02:05 -0600

    make the test work

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
@@ -8,18 +9,21 @@ module Main where
 
 
 import           Data.CountryCodes
+import           Data.Proxy
 import           Data.Set ( Set )
 import qualified Data.Set as S
 import           Data.Text
 import           Shpadoinkle hiding (name)
 import           Shpadoinkle.Backend.ParDiff
 import           Shpadoinkle.Html
+import           Shpadoinkle.Widgets.Types.Core
 import           Shpadoinkle.Widgets.Table
+import           StockName
 import           Test.QuickCheck
 
 
 data Sex = Male | Female
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
 
 data Person = Person
@@ -27,22 +31,30 @@ data Person = Person
             , age    :: Int
             , sex    :: Sex
             , origin :: CountryCode }
+  deriving (Eq, Show)
 
 
 data TableFilters = TableFilters
                   { bySex :: Maybe Sex
                   , byCountry :: Set CountryCode }
+  deriving (Eq, Show)
 
 
 data FilteredTable = FilteredTable
                    { contents :: [Person]
                    , filters  :: TableFilters }
+  deriving (Eq, Show)
 
 
 data instance Row FilteredTable = PersonRow { unRow :: Person }
 
 
 data instance Column FilteredTable = Name | Age | Sex | Origin
+  deriving (Eq, Ord, Show, Bounded, Enum)
+
+
+instance Humanize (Column FilteredTable) where
+  humanize = humanize . show
 
 
 instance Tabular FilteredTable where
@@ -63,11 +75,36 @@ instance Tabular FilteredTable where
       Sex    -> compareOn s (sex a)    (sex b)
       Origin -> compareOn s (origin a) (origin b)
 
+  toCell tab (unRow -> p) Name   = [text (name p)]
+  toCell tab (unRow -> p) Age    = [text (pack (show (age p)))]
+  toCell tab (unRow -> p) Sex    = [text (pack (show (sex p)))]
+  toCell tab (unRow -> p) Origin = [text (toName (origin p))]
 
-view :: () -> Html ()
-view _ = "hello world"
+
+instance Arbitrary Sex where
+  arbitrary = oneof [return Male, return Female]
+
+
+instance Arbitrary CountryCode where
+  arbitrary = elements (fst <$> allNames)
+
+
+instance Arbitrary Person where
+  arbitrary = Person <$> (unStockName <$> arbitrary) <*> choose (0,120) <*> arbitrary <*> arbitrary
+
+
+instance Arbitrary (Row FilteredTable) where
+  arbitrary = PersonRow <$> arbitrary
+
+
+genTable :: IO FilteredTable
+genTable = do
+  rows <- sequence .  Prelude.take 10000 $ repeat (generate arbitrary)
+  return (FilteredTable rows (TableFilters Nothing S.empty))
 
 
 main :: IO ()
-main = runJSorWarp 8080 $
-  simple runParDiff () (constly' . Main.view) getBody
+main = do
+  tab <- genTable
+  runJSorWarp 8080 $
+    simple runParDiff (tab, SortCol Name ASC) (uncurry (view Proxy)) getBody
